cb8ab3007e6ae24851d54ae418003582597584d3
Dereck Portela <dereck_alexander@outlook.com>
2024-09-14 12:26:10 -0300

    add loading handler

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,16 +3,20 @@ import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 import { useState, useEffect } from 'react';
+import PulseLoader from "react-spinners/PulseLoader";
 
 function PostList({ isModalOpen, onClose }){
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
     const url = 'http://127.0.0.1:8000/api/posts/';
 
     useEffect(() => {
         async function fetchPosts(){
+            setIsFetching(true);
             const response = await fetch(url);
             const responseData = await response.json();
             setPosts(responseData);
+            setIsFetching(false);
         }
 
         fetchPosts();
@@ -47,10 +51,21 @@ function PostList({ isModalOpen, onClose }){
                     {posts.map((post, index) => <Post key = { index } author = { post.author } body = { post.body } />)}
                 </ul> 
             }
-            {posts.length === 0 && 
+            {posts.length === 0 && !isFetching && 
                 <div className={classes.emptyMessage}>
                     <h1>There are no posts yet.</h1>
                     <h2>Don't be shy and share your thoughts!</h2>
+                </div>
+            }
+
+            {isFetching && 
+                <div className={ classes.loading }>
+                    <p>
+                        Loading posts
+                        <span className={ classes.pulseLoader }>
+                            <PulseLoader size={5} color='#dadada' />
+                        </span>
+                    </p>
                 </div>
             }
             
